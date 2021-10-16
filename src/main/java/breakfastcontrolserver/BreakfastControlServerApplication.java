946b408b06ef610e6f9556cee8899351970d3e5f
andreleao1 <andre.gustavo.ls@hotmail.com>
2021-10-16 10:53:32 -0300

    first commit

---
+++ src/main/java/breakfastcontrolserver/BreakfastControlServerApplication.java
@@ -0,0 +1,13 @@
+package breakfastcontrolserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakfastControlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakfastControlServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/breakfastcontrolserver/BreakfastControlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package breakfastcontrolserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakfastControlServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
